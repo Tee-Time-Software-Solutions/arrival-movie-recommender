d2ede023e08630f87f5c7acc6dbc86e96efdbe0a
javidominguezsegura <javier.dominguez.segura@gmail.com>
2026-02-20 12:20:05 +0100

    backend feedback API is done

--- backend/src/movie_recommender/services/recommender/main.py
+++ backend/src/movie_recommender/services/recommender/main.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from typing import List, Tuple
 
 from movie_recommender.schemas.users import UserPreferences
@@ -7,8 +8,8 @@ class Recommender:
     def __init__(self) -> None:
         pass
 
-    def get_top_n(
-        self, user_id: str, n: int, user_preferences: UserPreferences
+    def get_top_n_recommendations(
+        self, user_id: str, n: int, list_of_filtered_movies: List[int]
     ) -> List[Tuple[int, str]]:
         """
         Def: given a user_id and number of movies to retrieve it returns a list of IDs of movies. These movie IDs must
@@ -21,9 +22,30 @@ class Recommender:
         # Mock data
         return [(1, "Arrival"), (2, "Interstellar"), (3, "The Matrix")]
 
+    def get_similar_n_movies(self, movie_id: int, n: int):
+        """
+        Def: Give a movie_id returns its closes movies
+
+        TODO:
+            - Implement after MVP
+        """
+
     def get_user_feedback(
         self, user_id: str, movie_id: int, interaction_type, is_supercharged: bool
-    ) -> None: ...
+    ) -> None:
+        """
+        Belongs to:
+            - Online learning
+        """
+        ...
+
+    def _ingest_csv_dataset(self, csv_path: Path):
+        """
+        De:
+            Reads the CSV, filters for unique movie IDs, createes pairs of [(movie_id, movie_title)] then writes them to db
+        Belongs to:
+            - Offline learning
+        """
 
 
 # Add new function that id, movie_name out of ingested csv
