d5ad0c7a93919c1cc74a89f3e122a27e8a899f30
javidominguezsegura <javier.dominguez.segura@gmail.com>
2026-02-20 14:52:31 +0100

    feat: added all stateless app

--- backend/src/movie_recommender/services/recommender/main.py
+++ backend/src/movie_recommender/services/recommender/main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import List, Tuple
 
+from movie_recommender.schemas.interactions import SwipeAction
 from movie_recommender.schemas.users import UserPreferences
 
 
@@ -31,7 +32,11 @@ class Recommender:
         """
 
     def set_user_feedback(
-        self, user_id: str, movie_id: int, interaction_type, is_supercharged: bool
+        self,
+        user_id: str,
+        movie_id: int,
+        interaction_type: SwipeAction,
+        is_supercharged: bool,
     ) -> None:
         """
         Belongs to:
